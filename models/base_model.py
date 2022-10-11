558801cb5a6e06f3104a28bd33fa6af1316dd4d1
Malek Mayeh <loukatankionline@gmail.com>
2022-10-11 13:02:46 +0100

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -10,10 +10,10 @@ class BaseModel:
     """Base model class"""
     def __init__(self, *args, **kwargs):
         """__init__"""
+        date_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        date_format = "%Y-%m-%dT%H:%M:%S.%f"
 
         if kwargs:
             """key word arguements check & init attributes"""
