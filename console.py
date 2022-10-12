c33e17d781af0631834feee7991b01257b5c981c
Malek Mayeh <loukatankionline@gmail.com>
2022-10-12 10:49:48 +0100

    Console 01

--- console.py
+++ console.py
@@ -2,7 +2,9 @@
 """ console """
 
 
+from ast import arg
 import cmd
+from hashlib import new
 from models.base_model import BaseModel
 from models import storage
 
@@ -25,10 +27,47 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ Empty line should not execute anything """
         pass
+    
+    def do_create(self, args):
+        """Creates new instance of BaseModel"""
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.classes:
+            print("** class doesn't exist **")
+        else:
+            new_instance = eval(args)()
+            new_instance.save()
+            print(new_instance.id)
+
+    def do_show(self, args):
+        """Prints the string representation of an instance"""
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        if args[0] == 1:
+            print("** instance id missing **")
+            return
+        else:
+            total_obj = storage.all()
+            new_entry = args[0] + "." + args[1]
+            if new_entry in total_obj:
+                print(total_obj[new_entry])
+            else:
+                print("** no instance found **")
 
 
-
-
+    def do_destroy(self, args):
+        """Deletes an instance based on the class name and id"""
+    
+    def do_all(self, args):
+        """Prints all string representation of all instances """
+    
+    def do_update(self, args):
+        """Updates an instance based on the class name and id"""
+    
 
 
 
