680eefb2dfe8d4bed2b80f086c2aaab30fc72488
Malek Mayeh <loukatankionline@gmail.com>
2022-10-13 13:01:09 +0100

    Test Amenity

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""unittest Amenity Class"""
+
+
+
+from models.amenity import Amenity
+import uuid
+import unittest
+
+
+class Test_User(unittest.TestCase):
+    """Amenity Test"""
+     
+    def test_name_is_public_str(self):
+        self.assertEqual(str, type(Amenity.name))
