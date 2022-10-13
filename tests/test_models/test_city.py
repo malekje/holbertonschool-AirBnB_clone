2335585b8aa10b57e110c6e03a732eed52784d64
Malek Mayeh <loukatankionline@gmail.com>
2022-10-13 11:18:07 +0100

    Test City

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""unittest City Class"""
+
+from models.city import City
+import uuid
+import unittest
+
+class Test_User(unittest.TestCase):
+    """ Test class City """
+     
+    def test_state_id(self):
+        self.assertEqual(str, type(City.state_id))
+
+    def test_name(self):
+        self.assertEqual(str, type(City.name))
